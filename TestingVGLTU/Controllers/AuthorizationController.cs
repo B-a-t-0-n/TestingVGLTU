9be3fb908abea49b9619f3cb5ddb4d5ef317d4b2
Влад Книгин <vlaknig@gmail.com>
2024-07-06 15:05:58 +0300

    некоторые изменения

--- TestingVGLTU/Controllers/AuthorizationController.cs
+++ TestingVGLTU/Controllers/AuthorizationController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TestingVGLTU.Data;
 
 namespace TestingVGLTU.Controllers
 {
     public class AuthorizationController : Controller
     {
+        DataContext dataContext;
+
+        public AuthorizationController (DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
         public IActionResult Login()
         {
+            
             return View();
         }
     }
--- TestingVGLTU/Program.cs
+++ TestingVGLTU/Program.cs
@@ -1,16 +1,37 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using TestingVGLTU.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // получаем строку подключения из файла конфигурации
-string connection = builder.Configuration["DefaultConnection"]!;
+string connection = builder.Configuration.GetConnectionString("DefaultConnection")!;
 
 // добавляем контекст ApplicationContext в качестве сервиса в приложение
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(config =>
+{
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Login";
+    config.AccessDeniedPath = "/Auth/AccessDenied";
+});
+
+builder.Services.AddAuthorization(config =>
+{
+    config.AddPolicy("User", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+    });
+    config.AddPolicy("Admin", policy =>
+    {
+        policy.RequireClaim("IsAdmin", "True");
+    });
+});
 
 var app = builder.Build();
 
@@ -24,6 +45,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
