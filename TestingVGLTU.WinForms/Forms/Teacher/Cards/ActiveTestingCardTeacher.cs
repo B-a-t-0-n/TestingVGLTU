7c52ed327b8fe6ffc01f80e9011a3725286ee023
Влад Книгин <vlaknig@gmail.com>
2024-12-19 03:54:18 +0300

    добавлен дизайн для карточек тестирований

---
+++ TestingVGLTU.WinForms/Forms/Teacher/Cards/ActiveTestingCardTeacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingVGLTU.WinForms.Forms.Teacher.Cards
+{
+    public partial class ActiveTestingCardTeacher : UserControl
+    {
+        public ActiveTestingCardTeacher()
+        {
+            InitializeComponent();
+        }
+    }
+}
